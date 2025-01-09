d5a328ac525c3a8522cc7db38cb4996737ee0947
Reaz2020 <reaz.morshed@yahoo.com>
2025-01-09 23:24:23 +0100

    design changed for featured artifacts

--- src/components-home/FeaturedArtifacts.jsx
+++ src/components-home/FeaturedArtifacts.jsx
@@ -47,7 +47,7 @@ const FeaturedArtifacts = () => {
           <Loading />
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 ">
           {artifacts.map((artifact) => (
             <div
               key={artifact._id}
