3afc8675ea14d79de8b6c1d008e62bbfe1491dd2
Reaz2020 <reaz.morshed@yahoo.com>
2025-01-09 23:18:15 +0100

    4 products shown in the grid eintead of 3

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -63,7 +63,7 @@ const AllArtifacts = () => {
                 />
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" data-aos="zoom-in-up">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8" data-aos="zoom-in-up">
                 {artifacts.map((artifact) => (
                     <div
                         key={artifact._id}
